e4a57883253919e931ae13f273baf96a5d305b14
Abdulmalk Nkumbi <likmalik299@gmail.com>
2022-06-11 04:32:00 -0700

    LAB 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        String name = "Abdulmalk Nkumbi";
+        String registrationNumber = "2020-04-09798";
         double gravity = -9.81; // Earth's gravity in m/s^2
         double initialVelocity = 0.0;
         double fallingTime = 10.0;
         double initialPosition = 0.0;
-        double finalPosition = 0.0;
+        double finalPosition = 0.5 * gravity * fallingTime * fallingTime + initialVelocity * fallingTime + initialPosition;
         System.out.println("The object's position after " + fallingTime +
                 " seconds is " + finalPosition + " m.");
+        System.out.println(name + registrationNumber);
     }
 }
